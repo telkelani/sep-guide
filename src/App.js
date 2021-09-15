76c2ccd057a3ffeb5118e24c969cf72bcd854da2
telkelani <te00141@surrey.ac.uk>
2021-09-15 12:11:33 -0400

    removed jquery function .show

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ export default function App() {
       $('.sep-buttons').click(e => {
         console.log(e.target.textContent);
         selectSEP(e);
-        $('#modal').css('display', 'flex');
       });
       return;
     }
