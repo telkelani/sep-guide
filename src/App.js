16bc02acc42820aedb11d4817f426d99eb1ada68
telkelani <te00141@surrey.ac.uk>
2021-09-15 12:17:16 -0400

    window

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SEPModal from './SEPModal';
 export default function App() {
   var [showModal, setShowModal] = useState(false);
   var [selectedSEP, setselectedSEP] = useState(null);
+  window.$ = window.jQuery = require('jquery');
 
   window.onclick = e => {
     console.log('CLICKED');
