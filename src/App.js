edd721bfff542eba5fbee43ff6a24444bbafa065
telkelani <te00141@surrey.ac.uk>
2021-09-27 10:21:54 -0400

    Comments

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import SEPs from './SEPList';
 import SEPModal from './SEPModal';
 
 export default function App() {
+  //state variables for Modal
   var [showModal, setShowModal] = useState(false);
+
+  //State variable to indicate which SEP has been clicked
   var [selectedSEP, setselectedSEP] = useState(null);
 
+  //On Click handler to open modal
   var openModal = () => {
     setShowModal(() => {
       showModal = true;
@@ -14,32 +18,36 @@ export default function App() {
     });
   };
 
-  var closeModal = e => {
+  //OnClick handler to close modal
+  var closeModal = (e) => {
     setShowModal(() => {
       showModal = false;
       return showModal;
     });
   };
-  const selectSEP = e => {
+
+  //OnClick handler to select the correct SEP
+  const selectSEP = (e) => {
     openModal();
-    SEPs.map(SEP => {
+    SEPs.map((SEP) => {
       if (SEP.code === e.target.textContent) {
         setselectedSEP(SEP);
         return;
       }
     });
   };
+
   return (
     <div>
       <div id="title-section">
         <h1 className="page-title">SEP GUIDE</h1>
       </div>
       <div className="sep-group">
-        {SEPs.map(SEP => (
+        {SEPs.map((SEP) => (
           <button
             type="button"
             className="sep-buttons"
-            onClick={e => selectSEP(e)}
+            onClick={(e) => selectSEP(e)}
           >
             {SEP.code}
           </button>
--- src/SEPModal.js
+++ src/SEPModal.js
@@ -3,25 +3,36 @@ import React from 'react';
 import './sep.css';
 
 function SEPModal({ SEP, showModal, closeModal }) {
+
+  //Close Modal when clicking outside the box
+
+  //Make an onclick event on the window
   window.onclick = e => {
-    console.log(e.target.closest('#modal-content'));
+    console.log(e.target.closest('#modal-content')); 
     if (e.target.className == 'sep-buttons') {
       return;
     }
-    if (e.target.closest('#modal-content') == null) {
+    if (e.target.closest('#modal-content') == null) { //Closest indicates the how closely the element that the mouse had clicked relates to #modal-content. If it is not part of the modal it will be null
       closeModal(e);
     }
   };
+  
+  //When page first loads SEP will be null
   if (SEP === null) {
     return null;
   }
 
+  //If showModal is false do not return anything
   if (!showModal) {
     return null;
   }
+
+  //Check and crosses to see which SEP code can be used to enroll in which type of plan
   const PlanEnroll = () => {
     var elem = null;
     var style = { display: 'flex', justifyContent: 'center' };
+
+    //If can enroll in all 3
     if (SEP.enrollMA && SEP.enrollMAPD && SEP.enrollPDP) {
       elem = (
         <div style={style}>
@@ -30,7 +41,9 @@ function SEPModal({ SEP, showModal, closeModal }) {
           <h1>PDP ✔️</h1>
         </div>
       );
-    } else if (SEP.enrollMA && SEP.enrollMAPD) {
+    } 
+    //If can enroll in MA and MAPD but not PDP
+    else if (SEP.enrollMA && SEP.enrollMAPD) {
       elem = (
         <div style={style}>
           <h1>MA ✔️</h1>
@@ -38,7 +51,9 @@ function SEPModal({ SEP, showModal, closeModal }) {
           <h1>PDP ❌</h1>
         </div>
       );
-    } else if (SEP.enrollMA && SEP.enrollPDP) {
+    } 
+    //If can enroll in MA and PDP but not MAPD
+    else if (SEP.enrollMA && SEP.enrollPDP) {
       elem = (
         <div style={style}>
           <h1>MA ✔️</h1>
@@ -46,7 +61,9 @@ function SEPModal({ SEP, showModal, closeModal }) {
           <h1>PDP ✔️</h1>
         </div>
       );
-    } else if (SEP.enrollMAPD && SEP.enrollPDP) {
+    } 
+    //If can enroll in MAPD and PDP but not MA
+    else if (SEP.enrollMAPD && SEP.enrollPDP) {
       elem = (
         <div style={style}>
           <h1>MA ❌</h1>
@@ -54,7 +71,9 @@ function SEPModal({ SEP, showModal, closeModal }) {
           <h1>PDP ✔️</h1>
         </div>
       );
-    } else if (SEP.enrollMA) {
+    } 
+    //Just MA
+    else if (SEP.enrollMA) {
       elem = (
         <div style={style}>
           <h1>MA ✔️</h1>
@@ -62,7 +81,9 @@ function SEPModal({ SEP, showModal, closeModal }) {
           <h1>PDP ❌</h1>
         </div>
       );
-    } else if (SEP.enrollMAPD) {
+    }
+    //Just MAPD
+    else if (SEP.enrollMAPD) {
       elem = (
         <div style={style}>
           <h1>MA ❌</h1>
@@ -70,7 +91,9 @@ function SEPModal({ SEP, showModal, closeModal }) {
           <h1>PDP ❌</h1>
         </div>
       );
-    } else if (SEP.enrollPDP) {
+    } 
+    //Just PDP
+    else if (SEP.enrollPDP) {
       elem = (
         <div style={style}>
           <h1>MA ❌</h1>
